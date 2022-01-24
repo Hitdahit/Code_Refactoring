36de1e4e229ad8d7134513d07769e3b18c1b2f9b
Chang Yong Choi <ccd4565@gmail.com>
2022-01-24 19:47:52 +0900

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -13,44 +13,6 @@ import cv2
 from PIL import Image
 from torch.utils.data import Dataset
 
-# Make Dataset
-class Dataset(Dataset):
-    def __init__(self, img_dir, label_dir, data_type):
-        self.data      = glob.glob(os.path.join(img_dir, '*'))
-        self.label     = glob.glob(os.path.join(label_dir, '*'))
-        self.data_type = data_type
-
-        self.data_dictionary          = {}
-        self.data_dictionary['image'] = self.data
-        self.data_dictionary['label'] = self.label
-
-        self.transform = Albumentations()
-
-    def __len__(self):
-        return len(self.data)
-    
-    def __getitem__(self, idx):
-        if self.data_type == 'CT':
-            data = self.transform(CT_preprocess(self.data_dictionary['image'][idx]))*2 - 1 # value range : -1.0 ~ 1.0
-        elif self.data_type == 'X-ray':
-            data = self.transform(Xray_preprocess(self.data_dictionary['image'][idx]))*2 - 1 # value range : -1.0 ~ 1.0
-        else:
-            data = self.transform(Image.open(self.data_dictionary['image'][idx]))
-
-        label = self.data_dictionary['label'][idx]
-
-        data_dictionary = {'image' : data, 'label' : label}
-        data_dictionary = self.data_dictionary
-        return data_dictionary
-    
-
-# get loader from Dataset as batch size
-def get_loader(img_dir, label_dir, data_type, batch_size, workers):
-    dataset    = Dataset(img_dir, label_dir, data_type)
-    dataloader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, 
-                                             shuffle=True, num_workers=workers, drop_last=True)
-    return dataloader
-
 
 # Data transform
 def Albumentations():
@@ -102,3 +64,42 @@ def Xray_preprocess(data):
     np_image[np_image > 1] = 1
 
     return np_image # output : 0.0 ~ 1.0
+
+
+# Make Dataset
+class Dataset(Dataset):
+    def __init__(self, img_dir, label_dir, data_type):
+        self.data      = glob.glob(os.path.join(img_dir, '*'))
+        self.label     = glob.glob(os.path.join(label_dir, '*'))
+        self.data_type = data_type
+
+        self.data_dictionary          = {}
+        self.data_dictionary['image'] = self.data
+        self.data_dictionary['label'] = self.label
+
+        self.transform = Albumentations()
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        if self.data_type == 'CT':
+            data = self.transform(CT_preprocess(self.data_dictionary['image'][idx]))*2 - 1 # value range : -1.0 ~ 1.0
+        elif self.data_type == 'X-ray':
+            data = self.transform(Xray_preprocess(self.data_dictionary['image'][idx]))*2 - 1 # value range : -1.0 ~ 1.0
+        else:
+            data = self.transform(Image.open(self.data_dictionary['image'][idx]))
+
+        label = self.data_dictionary['label'][idx]
+
+        data_dictionary = {'image' : data, 'label' : label}
+        data_dictionary = self.data_dictionary
+        return data_dictionary
+    
+
+# get loader from Dataset as batch size
+def get_loader(img_dir, label_dir, data_type, batch_size, workers):
+    dataset    = Dataset(img_dir, label_dir, data_type)
+    dataloader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, 
+                                             shuffle=True, num_workers=workers, drop_last=True)
+    return dataloader
