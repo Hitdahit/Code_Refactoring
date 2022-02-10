4495481cbc35d1812c1cdc7ac79754af595d5ea8
SoyoungLee8 <lsy66939609@gmail.com>
2022-02-10 15:46:28 +0900

    중간수정입니다
    1. import torch
    2. import 형태 삼각으로
    3. model.to(device) 추가

--- train.py
+++ train.py
@@ -1,26 +1,63 @@
-import numpy as np
-import os
-
+import torch
+import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
+
+import os
+import numpy as np
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def binary_classification_train(model, loss_function, metric_function, optimizer, epochs):
-    train_dataloader = get_loader(train_image_dir, train_label_dir, data_type, batch_size, workers)
-    validation_dataloader = get_loader(validation_image_dir, validation_label_dir, data_type, batch_size, workers)
+# loss_functions
+nn.CrossEntropyLoss
+nn.BCELoss
+nn.BCEWithLogitsLoss
+
+'''
+TOBE
+'''
+def _parser(args):
+    ~~~
+    # loss path
+    if 'cross' in args.loss:
+        loss = nn.CrossEntropyLoss()
+    # 
+    if 'resnet50' in args.model:
+        model = model.resnet
+    
+    tr_path = args.tr_img_dir
+    
+    return loss, model, ...
+ 
+        
+def binary_classification_train(args):
+    model, loss_function, metric_function, optimizer, epochs = _parser(args)
+    ~~~
+
+'''
+present
+'''
+def binary_classification_train(model, loss_function, 
+                                metric_function, optimizer, epochs, train_image_dir, validation_image_dir):
+    train_dataloader = get_loader(train_image_dir, train_label_dir, 
+                                  data_type, batch_size, workers)
+    validation_dataloader = get_loader(validation_image_dir, validation_label_dir, 
+                                       data_type, batch_size, workers)
+    
 
     writer_train = SummaryWriter(logs_dir=os.path.join(logs_dir, 'train'))
     writer_val = SummaryWriter(logs_dir=os.path.join(logs_dir, 'val'))
 
     train_loss = []
     validation_loss = []
+    
+    model.to(device)
 
     for epoch in range(epochs):
         model.train()
         loss_epoch = []
         metric_epoch = []
 
-            # train
+        # train
         for index, batch in enumerate(train_dataloader):
             x = batch['image'].to(device)
             y = batch['label'].to(device)
@@ -34,8 +71,8 @@ def binary_classification_train(model, loss_function, metric_function, optimizer
             loss.backward()
             optimizer.step()
 
-            loss_epoch.append(loss.item())
-            metric_epoch.append(metric)
+            loss_epoch += loss.detach().cpu().numpy()
+            metric_epoch += metric.detach().cpu().numpy()
 
         # Tensorboard save
         writer_train.add_scalar('loss', np.mean(loss_epoch), epoch + 1)
@@ -58,8 +95,8 @@ def binary_classification_train(model, loss_function, metric_function, optimizer
                 loss = loss_function(yhat, y)
                 metric = metric_function(y, yhat)
 
-                loss_epoch.append(loss.item())
-                metric_epoch.append(metric)
+                loss_epoch += loss.detach().cpu().numpy()
+                metric = metric_function(y, yhat)
 
             # Tensorboard save
         writer_val.add_scalar('loss', np.mean(loss_epoch), epoch + 1)
