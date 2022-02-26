431272d179e619054dc925ad107a34bb003c6333
Park Ju Young <42925197+Hitdahit@users.noreply.github.com>
2022-02-26 16:12:24 +0900

    change set_value function
    set_value 는 본래 사용하던 버전,
    set_value2는 getattr를 사용하여 코드 길이를 줄인 버전. 이게 훨씬 간편한듯 한데 다들 어떠신지요

--- version_parser.py
+++ version_parser.py
@@ -183,7 +183,107 @@ class Version_Dictionary:          #class만들어서 하나하나 넣자  # 날
         }
 
         
+def set_value2(self):  # with getattr (str2attribute), locals, globals (str2namespace function)funciton
+        os.environ["CUDA_VISIBLE_DEVICES"]=self.GPU_number
+        self.n_classes = int(self.num_class)
+        self.device = torch.device(f'cuda:{int(self.GPU_id)}' if torch.cuda.is_available() else 'cpu') # CPU/GPU에서 돌아갈지 정하는 device flag
+        self.seed = int(self.seed)
+        self.batch_size = int(self.batch_size)
+        self.img_size = int(self.img_size)
+        self.epochs = int(self.epochs)
+        self.lr = float(self.leraning_rate)
 
+        '''
+        eps (float) -> for Adam family
+
+        lr_decay (float), weight_decay (float), initial_accumulator_value (int) -> for Adagrad
+
+        betas (tuple, float), weight_decay (float) -> for Adam
+
+        dampening (int) -> for SGD
+
+        '''
+        self.model = getattr(model, self.model)(self.n_classes, int(self.model_size))
+        self.loss = getattr(nn, self.loss)
+
+        '''
+        반드시 모든 파라미터들에 대한 값을 지정하게 해야할 것인가?
+        일단은 txt에 모든 파라미터 값을 지정해준다는 가정 (단 한줄에 다 때려박아서)하에 작성.
+            init에서 tuple로 만들어 줬다고 쳤을 때
+        만약 아니라면 파라미터값들을 하나씩 받게 하고 tuple로 만들어주는게 좋을 듯
+        ex. 
+            self.lr_decay = float(self.lr_decay) if self.lr_decay is not None else None
+            self.weight_decay = float(self.weight_decay) if self.weight_decay is not None else None
+            self.initial_accumulator_value = int(self.accumulator_value) if self.accumulator_value is not None else None
+            self.eps = float(self.eps) if self.eps is not None else None
+            self.betas = (float(self.betas1), float(self.betas2)) if self.betas1 is not None else None
+            self.dampening = int(self.dampening) if self.dampening is not None else None
+
+        '''
+        # optimizer TODO
+        '''
+        CLASS: torch.optim.Adagrad(params, lr=0.01, lr_decay=0, weight_decay=0, initial_accumulator_value=0, eps=1e-10)
+        https://pytorch.org/docs/stable/generated/torch.optim.Adagrad.html#torch.optim.Adagrad
+
+        CLASS torch.optim.Adam(params, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+        https://pytorch.org/docs/stable/generated/torch.optim.Adam.html#torch.optim.Adam
+
+        CLASS torch.optim.AdamW(params, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01, amsgrad=False)
+        https://pytorch.org/docs/stable/generated/torch.optim.AdamW.html#torch.optim.AdamW
+
+        CLASS torch.optim.SGD(params, lr=<required parameter>, momentum=0, dampening=0, weight_decay=0, nesterov=False)
+        https://pytorch.org/docs/stable/generated/torch.optim.SGD.html#torch.optim.SGD
+
+        if want to optimize two different network with one optimizer, then use itertools.chain().
+        '''
+        self.optimizer_params = tuple(list(self.optimizer_params).insert(0, self.model.parameters()))
+        self.optimizer = getattr(optim, self.optimizer)(*self.optimizer_params)
+
+        # scheduler TODO scheduler도 일단 optimizer와 같은 상황이라는 가정하에 작성.
+        '''
+        CLASS torch.optim.lr_scheduler.StepLR(optimizer, step_size, gamma=0.1, last_epoch=- 1, verbose=False)
+
+        CLASS torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones, gamma=0.1, last_epoch=- 1, verbose=False)
+        
+        CLASS torch.optim.lr_scheduler.ConstantLR(optimizer, factor=0.3333333333333333, total_iters=5, last_epoch=- 1, verbose=False)
+
+        CLASS torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=0.3333333333333333, end_factor=1.0, total_iters=5, last_epoch=- 1, verbose=False)
+
+        CLASS torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max, eta_min=0, last_epoch=- 1, verbose=False)
+
+        CLASS torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, threshold=0.0001, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08, verbose=False)
+        '''
+        self.scheduler_params = tuple(list(self.scheduler_params).insert(0, self.optimizer))
+        self.scheduler = getattr(lr_scheduler, self.scheduler)(*self.scheduler_params)
+
+        # augmentation TODO
+        '''
+        일단 augmentation은 getattr로 사용한다 해도 그 세팅값 전달을 어케할지가 고민..
+        일단 2key dictionary로 -> aug 종류 ('kind')(type:strings), 세팅 값('settings')(type:tuple)
+        이러면 아까와 또 마찬가지로 augmentation의 모든 인자값에 대한 parameter 를 txt에서 다 적어야함.
+        '''
+        aug_list = []
+        for i in self.augmentation_params:
+            aug_list.append(getattr(A, i['kind'])(*self.i['settings']))
+        self.augmentations = A.Compose(aug_list)
+
+        self.modality = self.modality   # TODO
+        self.dimension = int(self.dimension) #TODO
+
+        self.ckpt_dir = self.checkpoint_dir
+        self.log_dir = self.logs_dir
+        self.data_dir = self.data_dir
+
+        
+        self.task_type = self.task_type
+
+
+        self.epochs = self.epochs
+    
+        
+        
+        
+ 
 
  
  
