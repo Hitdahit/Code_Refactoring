12960e4123e4fa14a8cb4d39308ce8a99c41c2d2
JongJun Won <wjj910@gmail.com>
2022-04-14 21:28:51 +0900

    Update train.py
    Update multiclass train which deleted
    Add AMP(Automatic Mixed Precision) to all train methods

--- train.py
+++ train.py
@@ -51,7 +51,11 @@ def binary_classification_train(args):
     train_metric = []
     val_loss = []
     val_metric = []
-    
+
+    # select whether you use Automatic Mixed Precision or not
+    use_amp = True
+    scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
+
     model = args.model
     loss_function = args.loss()
     optimizer = args.optimizer
@@ -73,14 +77,23 @@ def binary_classification_train(args):
             y = batch['label'].to(args.device)
 
             # forward
-            yhat = model(x)
-            loss = loss_function(yhat, y)
+            with torch.cuda.amp.autocast(enabled=use_amp):
+                yhat = model(x)
+                loss = loss_function(yhat, y)
+                
             metric = BC_metric(y, yhat, args.logit_thresh)
             
             # backward
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            if use_amp:
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+                optimizer.zero_grad()
+
+            else:
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
             '''
             한 epoch 내의 loss, metric 값 추가
@@ -135,7 +148,7 @@ def binary_classification_train(args):
                 )
 
         # model, optimizer save all epoch
-        torch.save({'model': model.state_dict(), 'optimizer': optimizer.state_dict()},
+        torch.save({'model': model.state_dict(), 'optimizer': optimizer.state_dict(), 'scaler':  scaler.state_dict()},
        "./%s/model_epoch%d.pth.tar.gz" % (args.checkpoint_dir, epoch))
 
     writer_train.close()
@@ -189,6 +202,10 @@ def multilabel_classification_train(args):
     val_loss = []
     val_metric = []
 
+    # select whether you use Automatic Mixed Precision or not
+    use_amp = True
+    scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
+
     model = args.model
     loss_function = args.loss
     optimizer = args.optimizer
@@ -210,16 +227,24 @@ def multilabel_classification_train(args):
             y = data['label'].to(args.device) # multilabel_one_hot_encoding
 
             # forward
-            yhat = model(x)
-            loss = loss_function(yhat, y)
+            with torch.cuda.amp.autocast(enabled=use_amp):
+                yhat = model(x)
+                loss = loss_function(yhat, y)
 
             pred = yhat.round().detach().cpu().numpy() # 0.5이상 1, 0.5 미만 0으로 바꿔줌, gpu->cpu->numpy (accuracy 계산 할때 numpy로 했음)
             metric = ML_metric(y.detach().cpu().numpy(), pred) #accuracy 계산 코드 전부 numpy로 들어감
 
             # backward
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            if use_amp:
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+                optimizer.zero_grad()
+                
+            else:
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
             '''
             한 epoch 내의 loss, metric 값 추가
@@ -269,6 +294,141 @@ def multilabel_classification_train(args):
         val_loss.append(np.mean(loss_epoch))
         val_metric.append(metric_epoch / dataset_count) # 한 epoch 끝나면 accuracy 계산 
         
+        
+        # model, optimizer save all epoch
+        torch.save({'model': model.state_dict(), 'optimizer': optimizer.state_dict(), 'scaler':  scaler.state_dict()},
+       "./%s/model_epoch%d.pth.tar.gz" % (args.checkpoint_dir, epoch))
+        
+    writer_train.close()
+    writer_val.close()
+
+    return train_loss, train_metric, val_loss, val_metric
+
+
+'''
+multiclass
+'''
+
+def MC_metric(y, yhat):
+
+    acc_targets = []
+    acc_outputs = []
+
+    y_temp = y
+    for t in y_temp.view(-1,1).cpu():
+        acc_targets.append(t.item()) 
+
+    _, yhat_temp = torch.max(yhat, 1)
+    for u in yhat_temp.view(-1,1).cpu():
+        acc_outputs.append(u.item())
+
+    cor = 0
+    for i in range(len(acc_targets)):
+        if acc_outputs[i] == acc_targets[i]:
+            cor += 1
+
+    acc = cor/len(acc_outputs)
+
+    return acc
+
+
+def multiclass_classification_train(args):
+    '''
+    multiclass classification training&validation 함수
+    input: args
+    output: train_loss, train_metric, val_loss, val_metric
+    '''
+    # data loading
+    train_dataloader = get_loader(args, mode='train')
+    val_dataloader = get_loader(args, mode='valid')
+    
+    # 텐서보드를 사용하기 위한 SummaryWriter 설정, log 파일을 저장할 경로
+    writer_train = SummaryWriter(logs_dir=os.path.join(args.logs_dir, 'train'))
+    writer_val = SummaryWriter(logs_dir=os.path.join(args.logs_dir, 'val'))
+
+    # epoch 마다 하나씩 추가될 list들
+    train_loss = []
+    train_metric = []
+    val_loss = []
+    val_metric = []
+
+    # select whether you use Automatic Mixed Precision or not
+    use_amp = True
+    scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
+
+    model = args.model
+    loss_function = args.loss()
+    optimizer = args.optimizer
+    model.to(args.device) # model이 GPU에서 돌아가도록 설정
+
+    for epoch in range(args.epochs): # 0 ~ (epochs-1) 번의 for 문을 실행. for문의 index = epoch
+        model.train()
+        loss_epoch = []
+        metric_epoch = 0
+        dataset_count = 0 # accuracy 계산을 위한 data 갯수 count
+
+        for i, data in train_dataloader: # enumerate -> in
+            x = data['image'].to(args.device)
+            y = data['label'].to(args.device) 
+
+            # forward
+            with torch.cuda.amp.autocast(enabled=use_amp):
+                yhat = model(x)
+                loss = loss_function(yhat, y)
+            metric = MC_metric(y, yhat)
+
+            # backward
+            if use_amp:
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+                optimizer.zero_grad()
+                
+            else:
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            loss_epoch.append(loss.detach().cpu().numpy())
+            metric_epoch += metric
+            dataset_count += x.shape[0]
+
+        # scheduler.step()
+
+        # Tensorboard save
+        writer_train.add_scalar('loss', np.mean(loss_epoch), epoch + 1)
+        writer_train.add_scalar('accuracy', metric_epoch / dataset_count, epoch + 1)
+        
+        train_loss.append(np.mean(loss_epoch)) # 1 epoch 마다 train loss 추가
+        train_metric.append(metric_epoch / dataset_count) # 1 epoch 마다 train metric 추가
+        
+        # validation
+        with torch.no_grad():
+            model.eval()
+            val_loss_epoch = []
+            val_accuracy_epoch = 0 # iter당 accuracy 계산값
+            val_dataset_count = 0 # accuracy 계산을 위한 data 갯수 count
+
+            for data in val_dataloader: # enumerate -> in
+                # forward pass
+                x = data['image'].to(args.device)
+                y = data['label'].to(args.device) # multilabel_one_hot_encoding
+
+                yhat = model(x)
+
+                loss = loss_function(yhat, y)
+                metric = MC_metric(y, yhat)
+
+                loss_epoch.append(loss.detach().cpu().numpy())
+                metric_epoch += metric
+                dataset_count += x.shape[0]
+        
+        # Tensorboard save
+        writer_val.add_scalar('loss', np.mean(loss_epoch), epoch + 1)
+        writer_val.add_scalar('accuracy', metric_epoch / dataset_count, epoch + 1) #accuracy로 변경
+
+        val_loss.append(np.mean(loss_epoch))
+        val_metric.append(metric_epoch / dataset_count)
         
         # model, optimizer save all epoch
         torch.save({'model': model.state_dict(), 'optimizer': optimizer.state_dict()},
