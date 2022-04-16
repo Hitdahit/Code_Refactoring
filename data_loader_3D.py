14e96ff8793ac85a037c7d1c79c54ceae6438ae9
Chang Yong Choi <ccd4565@gmail.com>
2022-04-17 03:40:02 +0900

    CT 3D preprocess

--- data_loader_3D.py
+++ data_loader_3D.py
@@ -1,9 +1,9 @@
 import os
+from turtle import window_width
 import cv2
 import glob
 import pydicom
 import numpy as np
-import nibabel as nib
 
 import albumentations as A
 import albumentations.pytorch
@@ -11,10 +11,10 @@ import albumentations.pytorch
 import torch
 import torchvision
 from torch.utils.data import Dataset, DataLoader
-import util
+import util_3D
 
 # Make Dataset
-class Dataset(Dataset):
+class CT_Dataset(Dataset):
 
     def _BC_labeler(self):
         '''
@@ -37,13 +37,15 @@ class Dataset(Dataset):
         '''
         pass
 
-    def __init__(self, img_dir, mode, image_size, modality, transform):
+    def __init__(self, img_dir, mode, image_size, window_width, window_level, normalization, transform):
         '''
         data's root dir
         '''
         self.img_dir = os.path.join(img_dir, mode)
-        self.image_size = image_size # Get image_size
-        self.modality = modality        
+        self.image_size = image_size # Get image_size    
+        self.window_width = window_width # Get window_width
+        self.window_level = window_level # Get window_level
+        self.normalization = normalization
         self.transform = transform # Get augmentation
 
         self.classes = sorted(os.listdir(self.img_dir))
@@ -59,18 +61,13 @@ class Dataset(Dataset):
         img = self.imgs[idx] 
         label = self.labels[idx]
         image_size = self.image_size # Get image_size
-
+        window_width = self.window_width # Get window_width
+        window_level = self.window_level # Get window_level
+        normalization = self.normalization
+        
         # Pre-process task
-        if self.modality == 'CT':
-            img = util.CT_preprocess(img, image_size) # Data value range : 0.0 ~ 1.0
-            albu_dic = self.transform(image=img) # Apply augmentation to data
-        elif self.modality == 'Endo':
-            img = util.Endo_preprocess(img, image_size)*2-1
-            albu_dic = self.transform(image=img)
-        elif self.modality == 'MR':
-            '''
-            will be updated....
-            '''
+        img = util_3D.CT_preprocess(img, image_size, window_width, window_level, normalization) # Data value range : 0.0 ~ 1.0
+        albu_dic = self.transform(image=img) # Apply augmentation to data
 
         data_dic= {'image' : albu_dic['image'], 'label' : label} # Make dictionary which has one data to image key and one label to label key
         return data_dic
@@ -78,11 +75,20 @@ class Dataset(Dataset):
 
 # get loader from Dataset as batch size
 # img_dir, label_dir, preprocess_type, transform, batch_size, workers
-def get_loader(args, mode='train'):
-    dataset = Dataset(args.data_dir, mode, args.img_size, args.modality, args.augmentation)
-    dataloader = DataLoader(dataset=dataset, batch_size=args.batch_size,
-                            shuffle=args.shuffle, num_workers=args.num_worker,
-                            drop_last=args.drop_last)
+def get_loader(args, modality, mode='train'):
+    if modality == 'CT':
+        dataset = CT_Dataset(args.data_dir, mode, args.img_size, args.modality, args.augmentation)
+        dataloader = DataLoader(dataset=dataset, batch_size=args.batch_size,
+                                shuffle=args.shuffle, num_workers=args.num_worker,
+                                drop_last=args.drop_last)
+    elif modality == 'MR':
+        '''
+        will be updated...
+        '''
+    elif modality == 'Endo':
+        '''
+        will be updated...
+        '''
     return dataloader
 
 
--- util_3D.py
+++ util_3D.py
@@ -9,17 +9,48 @@ import nibabel as nib
 3D preprocessor도 util로 따로
 '''
 # CT pre-processing
-def CT_preprocess(data, image_size, window_width=None, window_level=None): # (input data, image size which you want to resize, window width which you want, window level which you want)
+def CT_preprocess(data, image_size, window_width=None, window_level=None, normalization='mean'): # (input data, image size which you want to resize, window width which you want, window level which you want)
     nifti_file = nib.load(data) # Read nifti file
-    nifti_np_array = nifti_file.get_fdata(dtype=np.float32) # shape(512,512,?)
+    nifti_np_array = nifti_file.get_fdata(dtype=np.float32)
 
+    # Alarm that data is empty.
     if nifti_np_array == 0:
         print('Data is empty. Data needs to be non-empty')
 
+    # CT image has Rescale Intercept and Rescale Slope. It is mandantory pre-process task.
     intercept = nifti_file.dataobj.inter
     slope = nifti_file.dataobj.slope
 
     nifti_np_array = nifti_np_array * slope + intercept
+    
+    # If you give a specific value in window_width and window_level, it will be windowed by value of those.
+    nifti_np_array = windowing(nifti_np_array, window_width, window_level)
+    
+    # Normalize intensity. There is two options, one is mean/std normalization and the other is min-max normalization.
+    nifti_np_array = normalize_intensity(nifti_np_array, normalization)
 
+    return nifti_np_array
 
-    return ddong
+def windowing(image, window_width=None, window_level=None):
+    if not window_width == None and window_level == None:
+        image_min = window_level - (window_width / 2.0)
+        image_max = window_level + (window_width / 2.0)
+
+        # If image pixel is over max value or under min value, threshold to max and min
+        image = np.clip(image, image_min, image_max)
+    else:
+        image_min = -1024.0
+        image_max = 3071.0
+
+        # If image pixel is over max value or under min value, threshold to max and min
+        image = np.clip(image, image_min, image_max)
+    return image
+
+def normalize_intensity(image, normalization='mean'):
+    if normalization == 'mean':
+        mean, std = np.mean(image), np.std(image)
+        image = (image - mean) / std
+    elif normalization == 'max':
+        min, max = np.min(image), np.max(image)
+        image = (image - min) / (max - min)
+    return image
