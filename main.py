bce76ec72f616553491dbd5df0ba92e5d3447ddd
hitdahit <godoctorsam@gmail.com>
2022-05-09 17:33:33 +0900

    mmaction2 configuration copy
    mmaction2 configuration copy.
    version_parser.py 대체제로 사용할 예정.
    
    아직 미완.

--- main.py
+++ main.py
@@ -18,6 +18,15 @@ txt_lst = get_version_text(setting_getter.version, setting_getter.text_path)
 args = Version_Dictionary(setting_getter.version, txt_lst)
 args.set_value()
 
+
+'''
+TODO: configs 나머지 코드에 녹이기
+from utils import configs
+setting_dict, setting_txt = configs.Config.fromfile('./settings.py')
+
+'''
+
+
 if 'BC' in args.task_type:
     train_loss, train_metric, val_loss, val_metric = binary_classification_train(args)
 elif 'MC' in args.task_type:
