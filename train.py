89b7ce212ac54636644520843a545a2effeea504
hitdahit <godoctorsam@gmail.com>
2022-06-26 14:36:31 +0900

    Need Update in train.py
    ENGINE - @Riveroad97
    kanggil_2 브랜치에 있는 engine 코드를 parser-v2 브랜치로 끌고 오려고 했는데
    그러면 컨플릭트 고치는게 더 오래 걸릴 것 같아서 여기 브랜치서 다시 커밋해주시는게 나을 것 같아요....
    
    TODO
    1. engine.py에 있던 코드 수정해서 현재 train.py에서 구현한 기능 모두 지원되도록 만들기.
    2. 현재 파일명 (train.py -> engine.py 로 변경하기.)

--- train.py
+++ train.py
@@ -375,3 +375,110 @@ def multiclass_classification_train(args):
     writer_val.close()
 
     return train_loss, train_metric, val_loss, val_metric
+
+
+
+'''
+
+import sys
+import torch
+from tqdm import tqdm as tqdm
+import numpy as np
+
+def format_logs(logs):
+    str_logs = ["{} - {:.4}".format(k, v) for k, v in logs.items()]
+    s = ", ".join(str_logs)
+    return s
+
+def train_epoch_classification(
+    model,
+    dataloader,
+    fn_loss,
+    metric,
+    optimizer,
+    device
+):
+    model.train()
+
+    logs = {}
+    loss_value = []
+    acc_scores = []
+
+    with tqdm(
+        dataloader,
+        desc='train',
+        file=sys.stdout,
+        disable= not True,
+        ncols=100
+    ) as iterator:
+        for data in iterator:
+            x = data['image'].float().to(device)
+            y = data['label'].long().to(device)
+
+            y_pred = model(x)
+
+            optimizer.zero_grad()
+
+            loss = fn_loss(y_pred, y).to(device)
+            loss_value.append(loss.item())
+            loss_logs = {'loss' : np.mean(loss_value)}
+            logs.update(loss_logs)
+            loss.backward()
+
+            acc = metric(y_pred, y).to(device)
+            acc_scores.append(acc.item())
+            metric_logs = {'acc' : np.mean(acc_scores)}
+            logs.update(metric_logs)
+
+            optimizer.step()
+
+            s = format_logs(logs)
+            iterator.set_postfix_str(s)
+
+    return logs
+
+
+@torch.no_grad()
+def valid_epoch_classification(
+    model,
+    dataloader,
+    fn_loss,
+    metric,
+    device
+):
+    model.eval()
+
+    logs = {}
+    loss_value = []
+    acc_scores = []
+
+    with tqdm(
+        dataloader,
+        desc='valid',
+        file=sys.stdout,
+        disable= not True,
+        ncols=100
+    ) as iterator:
+        for data in iterator:
+            x = data['image'].float().to(device)
+            y = data['label'].long().to(device)
+
+            y_pred = model(x)
+
+            loss = fn_loss(y_pred, y).to(device)
+            loss_value.append(loss.item())
+            loss_logs = {'loss' : np.mean(loss_value)}
+            logs.update(loss_logs)
+
+            acc = metric(y_pred, y).to(device)
+            acc_scores.append(acc.item())
+
+            metric_logs = {'acc' : np.mean(acc_scores)}
+            logs.update(metric_logs)
+
+            s = format_logs(logs)
+            iterator.set_postfix_str(s)
+
+    return logs
+    
+    '''
